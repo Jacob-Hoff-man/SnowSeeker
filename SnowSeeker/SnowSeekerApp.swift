134649ced21884b26c92b763a72cd0f994673878
Jacob Hoffman <cubhoffman@gmail.com>
2021-01-14 16:19:46 -0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Jacob Hoffman on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
